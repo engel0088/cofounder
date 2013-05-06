55c9c03760d89e65c4e671dacdd7442f80857006
engel lee <engel0088@gmail.com>
2013-05-06 19:05:32 +0800

    complete without banner slide

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,3 +1,13 @@
 class Member < ActiveRecord::Base
   attr_accessible :description, :industry, :level, :location, :lookfor, :name, :photo_url, :title
+
+  def self.pro
+     5
+  end
+
+  def self.new
+     1
+  end
+
+  
 end
