8f4cdebf786a7bc7822c588ccb6943c735d5e4bb
engel lee <engel0088@gmail.com>
2013-05-06 10:54:17 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Welcome Cofounderslab application.
+
+This is website source, created by Engel Lee in May 6, 2013.
+
+Version Control
+
+#Begin  at 2013/5/6
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cofounderlab::Application.config.secret_token = '4da35f277505663300fee07b3c6c67074bfb83be8e641464e15477ec4da32cc742e4426405804d470668e3a6412422080cd0d86b6dbad455dc80ea9d6f2770c4'
